7e54e01c8659cee4e2457712fae27d495abf94f5
Luís Soares <lsoares@gmail.com>
2021-02-24 19:09:21 +0000

    create base project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.30"
+}
+
+group = "com.luissoares"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
